aa6c531c30426e467308cf82a6321672a32f0db1
Filippos Vasilakis <vasilakisfil@gmail.com>
2012-10-02 00:20:36 +0300

    Update layout and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,20 +4,16 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'PhotoSharing App'" do
+    it "should have the h1 'EventDB'" do
       visit root_path
-      page.should have_selector('h1', :text => 'PhotoSharing App')
+      page.should have_selector('h1', :text => 'EventDB')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the title 'EventDB'" do
       visit root_path
-      page.should have_selector('title', :text => "")
+      page.should have_selector('title', :text => 'EventDB')
     end
 
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', text: '| Home')
-    end
   end
 
   describe "Help page" do
@@ -36,29 +32,29 @@ describe "Static pages" do
 
   describe "About page" do
 
-    it "should have the h1 'About Us'" do
+    it "should have the h1 'About EventDB'" do
       visit about_path
-      page.should have_selector('h1', :text => 'About Us')
+      page.should have_selector('h1', :text => 'About EventDB')
     end
 
-    it "should have the title 'About Us'" do
+    it "should have the title 'About EventDB'" do
       visit about_path
       page.should have_selector('title',
-                    :text => "About Us")
+                    :text => "About EventDB")
     end
   end
 
   describe "Contact page" do
 
-    it "should have the h1 'Contact'" do
+    it "should have the h1 'Contact Us'" do
       visit contact_path
-      page.should have_selector('h1', text: 'Contact')
+      page.should have_selector('h1', text: 'Contact Us')
     end
 
-    it "should have the title 'Contact'" do
+    it "should have the title 'Contact Us'" do
       visit contact_path
       page.should have_selector('title',
-                    text: "Contact")
+                    text: "Contact Us")
     end
   end
 end

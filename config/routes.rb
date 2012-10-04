268ca3ccbf5f4d7c9a2d82b634c8fd7d6b8ae887
Filippos Vasilakis <vasilakisfil@gmail.com>
2012-10-04 23:57:25 +0300

    Make a basic User model (including secure password)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 PhotosharingApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
+
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/help', to: 'static_pages#help'
+
 
 
 

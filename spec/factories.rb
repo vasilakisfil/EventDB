d5b35037df984482d1aed6b4fc6a58a2b5421295
Filippos Vasilakis <vasilakisfil@gmail.com>
2012-10-08 02:19:12 +0300

    Update user in factory girl

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name "Filippos Vasilakis"
-    email "vasilakisfil@gmail.com"
+    name "example TheExample"
+    email "example@example.com"
+    username "The Dude"
     password "foobar"
     password_confirmation "foobar"
   end

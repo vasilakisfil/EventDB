c5a301f9123b2447b49121f2e4554af96fedd441
Filippos Vasilakis <vasilakisfil@gmail.com>
2012-11-12 23:11:49 +0200

    Fix delete links (back in present !!)

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -36,4 +36,16 @@ describe "MicropostPages" do
       end
     end
   end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit user_path(user) }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
+  end
 end
